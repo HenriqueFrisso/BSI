0ee9847328a15226de37bd61df9f02a928d5978e
HenriqueFrisso <henriquefifes@gmail.com>
2024-11-07 11:06:45 -0300

    Adições da manhã

--- AgeOfMythology/src/ageofmythology/Arena.java
+++ AgeOfMythology/src/ageofmythology/Arena.java
@@ -1,21 +1,26 @@
 package ageofmythology;
 
 import ageofmythology.guerreiros.Guerreiro;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Arena {
-    LinkedList<LinkedList<Guerreiro>> lado1 = new LinkedList<>();
-    LinkedList<LinkedList<Guerreiro>> lado2 = new LinkedList<>();
+    List<String> arquivosLado1 = List.of("lado11.txt", "lado12.txt", "lado13.txt", "lado14.txt");
+    List<String> arquivosLado2 = List.of("lado21.txt", "lado22.txt", "lado23.txt", "lado24.txt");
     
-    LinkedList<Guerreiro> lista11 = new LinkedList<>();
-    LinkedList<Guerreiro> lista12 = new LinkedList<>();
-    LinkedList<Guerreiro> lista13 = new LinkedList<>();
-    LinkedList<Guerreiro> lista14 = new LinkedList<>();
     
-    LinkedList<Guerreiro> lista21 = new LinkedList<>();
-    LinkedList<Guerreiro> lista22 = new LinkedList<>();
-    LinkedList<Guerreiro> lista23 = new LinkedList<>();
-    LinkedList<Guerreiro> lista24 = new LinkedList<>();
     
+    ArrayList<LinkedList<Guerreiro>> lado1 = new ArrayList<>();
+    ArrayList<LinkedList<Guerreiro>> lado2 = new ArrayList<>();
+    
+      
+    private void iniciarLados(ArrayList<LinkedList<Guerreiro>> Arraylado, List arquivos, int lado){
+        for (int i = 0; i < arquivos.size(); i++) {
+            LinkedList<Guerreiro> linkedList = new LinkedList<>();
+            lerArquivo(linkedList);
+            Arraylado.add(linkedList);
+        } 
+    }
     
 }
