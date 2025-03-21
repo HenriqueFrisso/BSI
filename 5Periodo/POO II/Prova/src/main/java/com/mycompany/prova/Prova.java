c4257eb6868a82d18c5c8f2c72409ccd32481229
Henrique Frisso Oliveira <2023122760018@cefetes.br>
2025-03-21 14:42:15 -0300

    trab

---
+++ 5Periodo/POO II/Prova/src/main/java/com/mycompany/prova/Prova.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prova;
+
+/**
+ *
+ * @author 2023122760018
+ */
+public class Prova {
+
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+        tela.setVisible(true);
+                
+    }
+}
---
+++ 5Periodo/POO II/trab/TrabalhoPOO/src/main/java/com/mycompany/trabalhopoo/TrabalhoPOO.java
@@ -0,0 +1,11 @@
+package com.mycompany.trabalhopoo;
+
+import com.mycompany.trabalhopoo.telas.*;
+
+public class TrabalhoPOO {
+
+    public static void main(String[] args) {
+        TelaInicial tela = new TelaInicial();
+        tela.setVisible(true);
+    }
+}
